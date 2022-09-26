9b35e698c385c8f84d82292e17e65aaceef75152
fajarfadillahh <fajaragustian979@gmail.com>
2022-09-26 12:42:49 +0700

    updated darkmode-website-tailwind-vite

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,11 @@ import "../css/index.css";
 
 var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
 var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+
 // change the icons inside the button based on previous settings
 if (
-  localStorage.getItem("color-theme") === "dark" ||
-  (!("color-theme" in localStorage) &&
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
     window.matchMedia("(prefers-color-scheme: dark)").matches)
 ) {
   themeToggleLightIcon.classList.remove("hidden");
@@ -15,29 +16,30 @@ if (
 
 // theme toggle button
 var themeToggleBtn = document.getElementById("theme-toggle");
-themeToggleBtn.addEventListener("click", function () {
+
+themeToggleBtn.addEventListener("click", () => {
   // toggle icons inside button
   themeToggleLightIcon.classList.toggle("hidden");
   themeToggleDarkIcon.classList.toggle("hidden");
 
   // if set via local storage previously
-  if (localStorage.getItem("color-theme")) {
-    if (localStorage.getItem("color-theme") === "light") {
+  if (localStorage.theme) {
+    if (localStorage.getItem("theme") === "light") {
       document.documentElement.classList.add("dark");
-      localStorage.setItem("color-theme", "dark");
+      localStorage.setItem("theme", "dark");
     } else {
       document.documentElement.classList.remove("dark");
-      localStorage.setItem("color-theme", "light");
+      localStorage.setItem("theme", "light");
     }
 
     // if NOT set via local storage previously
   } else {
     if (document.documentElement.classList.contains("dark")) {
       document.documentElement.classList.remove("dark");
-      localStorage.setItem("color-theme", "light");
+      localStorage.setItem("theme", "light");
     } else {
       document.documentElement.classList.add("dark");
-      localStorage.setItem("color-theme", "dark");
+      localStorage.setItem("theme", "dark");
     }
   }
 });
